a0e634037760241d85c1841f39fde388c970be22
MUHAMMAD TAHA <160233035+tahaawan123@users.noreply.github.com>
2024-03-02 20:33:01 +0500

    Add files via upload

---
+++ arithmeticoperators/subtract.js
@@ -0,0 +1,5 @@
+"use strict";
+// subtraction 
+let totalAmount = 100;
+let spendAmount = 30;
+console.log(totalAmount - spendAmount);
---
+++ arithmeticoperators/subtract.ts
@@ -0,0 +1,4 @@
+// subtraction 
+let totalAmount = 100;
+let  spendAmount = 30;
+console.log(totalAmount - spendAmount);
