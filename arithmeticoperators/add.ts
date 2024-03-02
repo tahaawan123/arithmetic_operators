d87abbd7e7ce6b70e4f8514da506f9935b9b1074
MUHAMMAD TAHA <160233035+tahaawan123@users.noreply.github.com>
2024-03-02 20:16:37 +0500

    Add files via upload

---
+++ arithmeticoperators/add.ts
@@ -0,0 +1,14 @@
+// Addition Example.
+let num1 = 20;
+let num2 = 30;
+console.log(num1 + num2);
+
+// this value can't be add because num3 variable is in string form.
+   let num3 ="20";
+   let num4 =30;
+   console.log(num3 + num4);
+
+      let num5 = 20;
+      let num6 = 30;
+      let num7 = 50;
+      console.log(num5 + num6 + num7); 
