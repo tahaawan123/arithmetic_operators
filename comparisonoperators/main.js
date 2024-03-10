d874849cf0db8306f56be61ab7e0d2e7219b58bc
MUHAMMAD TAHA <160233035+tahaawan123@users.noreply.github.com>
2024-03-11 03:09:26 +0500

    Add files via upload

---
+++ comparisonoperators/main.js
@@ -0,0 +1,26 @@
+"use strict";
+// Comparison operators
+let a = 10;
+let b = 12;
+let c = "10";
+let d = 30;
+let e = 12;
+// (==) Equality Operator compares two values for equality.
+console.log(b == e);
+// (===) Strict Equality Operator mean that  both the value and the type of the operand must be the same
+console.log(b === e);
+// if either the value or the type of the operand is  different the Strict Equality operator will return false
+console.log(a === c);
+// (>)Greater Than OPerator 
+// returns (True) if the left operand is greater than the right operand.
+console.log(b > a);
+// (>=) Greter Than OR Equal to 
+console.log(d >= e);
+console.log(e >= b);
+//   (<) Less Than 
+console.log(b < d);
+// (<=)Less Than OR Equal to
+console.log(b <= e);
+console.log(b <= d);
+// (!=) Not equal to
+console.log(d != e);
