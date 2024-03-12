98f13895bdcb91f7694b72575078f72a209872d8
MUHAMMAD TAHA <160233035+tahaawan123@users.noreply.github.com>
2024-03-12 16:18:13 +0500

    Add files via upload

---
+++ assignment_operator/main.js
@@ -0,0 +1,10 @@
+"use strict";
+// Assignment Operator are used to assigning values to variables.
+let a = 10;
+let b = 20;
+a += b;
+console.log(a);
+a += b;
+console.log(a);
+a -= b;
+console.log(a);
---
+++ assignment_operator/main.ts
@@ -0,0 +1,10 @@
+// Assignment Operator are used to assigning values to variables.
+let a = 10;
+let b =20;
+
+a+=b;
+console.log(a);
+a+=b;
+console.log(a);
+a-=b;
+console.log(a);
